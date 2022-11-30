f0576c654bdc83f750c639e7ebbaa2084abf23b2
Surya Teja <teja8551@gmail.com>
2022-11-30 23:25:49 +0530

    Adjusted Scripts

--- Assets/Scripts/CreateObjects.cs
+++ Assets/Scripts/CreateObjects.cs
@@ -8,6 +8,48 @@ public class CreateObjects : MonoBehaviour
     // public GameObject CubePrefab;
     public List<GameObject> ObjectShapes;
     public GameObject objects;
+    private GameObject WelcomePanel;
+    private GameObject InGamePanel;
+    private GameObject GameOverPanel;
+
+    private void Start()
+    {
+        WelcomePanel = GameObject.Find("WelcomePanel");
+        InGamePanel = GameObject.Find("InGamePanel");
+        GameOverPanel = GameObject.Find("GameOverPanel");
+
+        WelcomePanel.SetActive(true);
+        InGamePanel.SetActive(false);
+        GameOverPanel.SetActive(false);
+    }
+
+    private bool isColorGreen(string objName)
+    {
+        Material status = GameObject.Find(objName).transform.Find("left").GetComponent<Renderer>().material;
+        return status.name.Split(" ")[0] == "Green";
+    }
+
+    private bool IsGameOver()
+    {
+
+        return isColorGreen("CubeBin") &&
+               isColorGreen("CuboidBin") &&
+               isColorGreen("ConeBin") &&
+               isColorGreen("CylinderBin") &&
+               isColorGreen("PyramidBin") &&
+               isColorGreen("SphereBin");
+    }
+
+    private void Update()
+    {
+        if (IsGameOver())
+        {
+            WelcomePanel.SetActive(false);
+            InGamePanel.SetActive(false);
+            GameOverPanel.SetActive(true);
+        }
+    }
+
     IEnumerator WaitForNext(int i)
     {
         if (i < ObjectShapes.Count)
@@ -15,13 +57,22 @@ public class CreateObjects : MonoBehaviour
             yield return new WaitForSeconds(0.3f);
             GameObject clone = Instantiate(ObjectShapes[i], transform.position, Quaternion.identity);
             clone.transform.parent = objects.transform;
-            clone.name = ObjectShapes[i].name + "_" + i;
+            clone.name = ObjectShapes[i].name;
             StartCoroutine(WaitForNext(i + 1));
         }
     }
+
     public void startGame()
     {
         Debug.Log("Game Started");
         StartCoroutine(WaitForNext(0));
+        WelcomePanel.SetActive(false);
+        InGamePanel.SetActive(true);
+        GameOverPanel.SetActive(false);
+    }
+
+    public void RetryGame()
+    {
+
     }
 }
--- Assets/Scripts/CubeDetector.cs
+++ Assets/Scripts/CubeDetector.cs
@@ -1,28 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CubeDetector : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Material GreenMaterial;
+    public Material RedMaterial;
+    public Material WhiteMaterial;
+    Dictionary<string, bool> cubes;
+    Dictionary<string, bool> others;
     void Start()
     {
+        cubes = new Dictionary<string, bool>();
+        others = new Dictionary<string, bool>();
 
+        cubes["Dice"] = false;
+        cubes["RubiksCube"] = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Renderer CubeRend = GameObject.Find("CubeBin").transform.Find("left").GetComponent<Renderer>();
+        if (others.Count != 0)
+        {
+            CubeRend.material = RedMaterial;
+            return;
+        }
+        List<string> keys = cubes.Keys.ToList();
+        foreach (var key in keys)
+        {
+            if (!cubes[key])
+            {
+                CubeRend.material = WhiteMaterial;
+                return;
+            }
+        }
+        CubeRend.material = GreenMaterial;
+        return;
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        print(other.gameObject.name + "Enter");
+        string name = other.gameObject.name;
+        if (cubes.ContainsKey(other.gameObject.name))
+        {
+            cubes[name] = true;
+        }
+        else
+        {
+            others[name] = true;
+        }
     }
 
     public void OnTriggerExit(Collider other)
     {
-        print(other.gameObject.name + "Exit");
+        string name = other.gameObject.name;
+        if (cubes.ContainsKey(other.gameObject.name))
+        {
+            cubes[name] = false;
+        }
+        else
+        {
+            others.Remove(name);
+        }
     }
 }
